948e6e60a55a034a00f1a83f93b8e3c1859f011e
Igor Shevchenko <hello.igorshevchenko@gmail.com>
2020-12-30 22:02:15 -0500

    added win condition

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 15;
+const cells = 3;
 const width = 600;
 const height = 600;
 
 const unitLength = width / cells;
 
 const engine = Engine.create();
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
   element: document.body,
@@ -155,14 +156,17 @@ const goal = Bodies.rectangle(
   unitLength * 0.7,
   unitLength * 0.7,
   {
-    isStatic: true
+    isStatic: true,
+    label: "goal"
   }
 );
 World.add(world, goal);
 
-// BAll
+// Ball
 
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+  label: "ball"
+});
 World.add(world, ball);
 
 document.addEventListener("keydown", (event) => {
@@ -182,4 +186,18 @@ document.addEventListener("keydown", (event) => {
   if (event.keyCode === 65) {
     Body.setVelocity(ball, { x: x - 5, y });
   }
+});
+
+// Win Condition
+
+Events.on(engine, "collisionStart", (event) => {
+  event.pairs.forEach((collision) => {
+    const labels = ["ball", "goal"];
+    if (
+      labels.includes(collision.bodyA.label) &&
+      labels.includes(collision.bodyB.label)
+    ) {
+      console.log("User won!");
+    }
+  });
 });
