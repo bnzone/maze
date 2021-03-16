a306df6f892cda9f9c47b00b8661da4ee7d684aa
Igor Shevchenko <bnzone@gmail.com>
2021-03-16 11:55:12 -0400

    change difficulty

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cellsHorizontal = 16;
-const cellsVertical = 12;
+const cellsVertical = 14;
 
 const width = window.innerWidth;
 const height = window.innerHeight;
